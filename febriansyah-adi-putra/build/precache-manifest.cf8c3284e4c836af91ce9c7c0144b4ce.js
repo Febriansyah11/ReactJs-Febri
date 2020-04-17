e13f04dca2fd83950ff582c7ec0ba643634fdfc4
Febriansyah11 <1101.adiputra@gmail.com>
2020-04-18 06:17:03 +0700

    first commit

---
+++ febriansyah-adi-putra/build/precache-manifest.cf8c3284e4c836af91ce9c7c0144b4ce.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b301c5b8522a8c899ddbc24499949bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32097a1fdb4b06ce1d3d",
+    "url": "/static/css/2.7643b639.chunk.css"
+  },
+  {
+    "revision": "6d6a5d07b76955a2b4df",
+    "url": "/static/css/main.a0b84ec9.chunk.css"
+  },
+  {
+    "revision": "32097a1fdb4b06ce1d3d",
+    "url": "/static/js/2.a324525e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a324525e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6a5d07b76955a2b4df",
+    "url": "/static/js/main.e4b372f3.chunk.js"
+  },
+  {
+    "revision": "621a5c449f8c246ebd47",
+    "url": "/static/js/runtime-main.dd0b20bc.js"
+  },
+  {
+    "revision": "ad3904b12feba1d6a8675f82f02241bc",
+    "url": "/static/media/Github.ad3904b1.svg"
+  },
+  {
+    "revision": "f49f3d2d9df5013c9bfaab7e3d39ee57",
+    "url": "/static/media/Raleway-Bold.f49f3d2d.ttf"
+  },
+  {
+    "revision": "be3bf63a30b4523ae221bd4358b13e8f",
+    "url": "/static/media/Raleway-ExtraBold.be3bf63a.ttf"
+  },
+  {
+    "revision": "466d154fedd98f85c9deb363ccf859a7",
+    "url": "/static/media/Raleway-Light.466d154f.ttf"
+  },
+  {
+    "revision": "e2a70086178378a6165ad7b032ee1077",
+    "url": "/static/media/Raleway-LightItalic.e2a70086.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "5a25c50b181b07279489ce5bb6a9545c",
+    "url": "/static/media/Raleway-SemiBold.5a25c50b.ttf"
+  },
+  {
+    "revision": "6f7a2864464eefc53e89bb45955663bb",
+    "url": "/static/media/blog.6f7a2864.png"
+  },
+  {
+    "revision": "50cfb66b3e44c31020f5db6cd44cddf0",
+    "url": "/static/media/blogger.50cfb66b.mp4"
+  },
+  {
+    "revision": "3926a3f8411afa7fd26b6a39c995f363",
+    "url": "/static/media/futsal0.3926a3f8.png"
+  },
+  {
+    "revision": "30f7b9d75ef0f796ac693ac495464489",
+    "url": "/static/media/futsal1.30f7b9d7.png"
+  },
+  {
+    "revision": "e520ac20c0d77b9c3171cca0413b8615",
+    "url": "/static/media/futsal2.e520ac20.png"
+  },
+  {
+    "revision": "26848c33294534bd65c0f25549d82dc4",
+    "url": "/static/media/futsalBg1.26848c33.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74951ae4439450044d3ab4d68df11609",
+    "url": "/static/media/photo.74951ae4.svg"
+  }
+]);
