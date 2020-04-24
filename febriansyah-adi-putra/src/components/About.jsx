13d9842afb4896ff7baca9db8456c9b447a999cf
Febriansyah11 <1101.adiputra@gmail.com>
2020-04-24 11:52:18 +0700

    Edit text about

--- febriansyah-adi-putra/src/components/About.jsx
+++ febriansyah-adi-putra/src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="mt-xl-5 mt-lg-5">
             <p className="text-raleway-about text-center">
-                I'am a developer, my first studied at Balai Latihan Kerja Bekasi&nbsp;<a href="https://blkbekasi.kemnaker.go.id/" className="text-raleway-about-italic">BBPLK</a>. I majored in Web Developer, there i learned HTML and PHP Native. After that, i got more training again at PT. Enigma Humanika Cipta, there i learn to be Fullstack Developer, all for free!. And now i've mastered several frameworks and programming languages like a
+                I'am a developer, my first studied at Balai Latihan Kerja Bekasi&nbsp;<a href="https://blkbekasi.kemnaker.go.id/" className="text-raleway-about-italic">BBPLK</a>. I majored in Web Developer, there i learned HTML and PHP Native. After that, i got more training again at PT. Enigma Humanika Cipta, there i learn to be Fullstack Developer, all for free!. And now i've understand several frameworks and programming languages like a
             </p>
             <p className="text-raleway-about-item p-0 mt-xl-5 mt-lg-5 mt-md-2 mt-1 text-center">
                 Javascript ~ ReactJs ~ ReactNative ~ HTML5 ~ CSS3 ~ Bootstrap4
